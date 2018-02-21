a058d51a0caca994eb58ce60a27fe0017f7b4e61
Matheus <matheus510.fonseca@gmail.com>
2018-02-21 10:55:47 -0300

    modais implementadas (5 tipos)

--- src/components/ui/PortalModal.vue
+++ src/components/ui/PortalModal.vue
@@ -1,25 +1,33 @@
 <template>
-  <v-layout row justify-center>
-    <v-dialog v-model="dialog" persistent max-width="290">
-      <v-btn color="primary" dark slot="activator">Open Dialog</v-btn>
-      <v-card>
-        <v-card-title class="headline">Use Google's location service?</v-card-title>
-        <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="green darken-1" flat @click.native="dialog = false">Disagree</v-btn>
-          <v-btn color="green darken-1" flat @click.native="dialog = false">Agree</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-layout>
+  <v-dialog v-model="modalContent.active" persistent max-width="500px">
+    <v-btn color="primary" dark slot="activator">Open Dialog</v-btn>
+    <v-card>
+      <v-card-title :class="{
+        success: modalContent.type === 'success',
+        primary: modalContent.type === 'primary',
+        info: modalContent.type === 'info',
+        warning: modalContent.type === 'warning',
+        danger: modalContent.type === 'danger'
+      }"
+      >
+        <span class="headline">{{ modalContent.title }}</span>
+      </v-card-title>
+      <v-card-text>
+       {{ modalContent.content }}
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" flat @click.native="modalContent.active = false">{{ modalContent.confirmButton.text }}</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 export default {
   props: {
     modalObject: {
-      type: Array,
+      type: Object,
       required: true
       // falta validação.
     }
@@ -27,35 +35,36 @@ export default {
 
   data () {
     return {
-      modalContent: {
-        type: ['success', 'info', 'warning', 'error'],
-        isForm: false,
-        title: '',
-        content: '',
-        actions: [
-          {
-            actionName: 'Salvar',
-            color: '',
-            onClick: ''
-          },
-          {
-            actionName: 'Editar',
-            color: '',
-            onClick: ''
-          },
-          {
-            actionName: 'Excluir',
-            color: '',
-            onClick: ''
-          }
-        ]
-      },
-      dialog: false
+      modalContent: this.modalObject
     }
   }
 }
 </script>
 
 <style>
-
+.success {
+  color: #fff;
+  background-color: #5cb85c;
+  border-color: #4cae4c;
+}
+.primary {
+  color: #fff;
+  background-color: #428bca;
+  border-color: #357ebd;
+}
+.info {
+  color: #fff;
+  background-color: #5bc0de;
+  border-color: #46b8da;
+}
+.warning {
+  color: #fff;
+  background-color: #f0ad4e;
+  border-color: #eea236;
+}
+.danger {
+  color: #fff;
+  background-color: #d9534f;
+  border-color: #d43f3a;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import Home from '@/pages/Home'
 import Login from '@/pages/Login'
 import Grid from '@/pages/Grid'
+import Dialog from '@/pages/Dialog'
 
 Vue.use(Router)
 
@@ -23,6 +24,11 @@ export default new Router({
       path: '/home',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/dialog',
+      name: 'Dialog Test',
+      component: Dialog
     }
   ]
 })
