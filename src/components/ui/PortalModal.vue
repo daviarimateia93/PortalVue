bfdf97a6703b8793fdce230d4bf8d144b0c55861
Matheus <matheus510.fonseca@gmail.com>
2018-02-26 09:56:36 -0300

    pequeno concerto no modal

--- src/components/ui/PortalModal.vue
+++ src/components/ui/PortalModal.vue
@@ -1,7 +1,7 @@
 <template>
   <v-dialog v-model="modalContent.active" persistent max-width="500px">
     <v-card>
-      <v-card-title :class="modalTypeClass">
+      <v-card-title :class="modalContent.type" class="white--text">
         <span class="headline">{{ modalContent.title }}</span>
       </v-card-title>
       <v-card-text>
@@ -9,7 +9,7 @@
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn :class="modalTypeClass" flat @click.native="modalContent.active = false">{{ modalContent.confirmButton.text }}</v-btn>
+        <v-btn :class="modalContent.type" class="white--text" flat @click.native="modalContent.active = false">{{ modalContent.confirmButton.text }}</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -29,40 +29,9 @@ export default {
     return {
       modalContent: this.modalObject
     }
-  },
-
-  computed: {
-    modalTypeClass () {
-      return this.modalContent.type
-    }
   }
 }
 </script>
 
 <style>
-.success {
-  color: #fff !important;
-  background-color: #5cb85c !important;
-  border-color: #4cae4c;
-}
-.primary {
-  color: #fff !important;
-  background-color: #428bca !important;
-  border-color: #357ebd;
-}
-.info {
-  color: #fff !important;
-  background-color: #5bc0de !important;
-  border-color: #46b8da;
-}
-.warning {
-  color: #fff !important;
-  background-color: #f0ad4e !important;
-  border-color: #eea236;
-}
-.danger {
-  color: #fff !important;
-  background-color: #d9534f !important;
-  border-color: #d43f3a;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -12,7 +12,15 @@ import { store } from './store/store'
 
 moment.locale('pt-br')
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#3f51b5',
+    secondary: '#b0bec5',
+    success: '#7CB342',
+    alert: '#FF9100',
+    danger: '#E53935'
+  }
+})
 
 Vue.config.productionTip = false
 
